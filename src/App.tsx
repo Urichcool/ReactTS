a9176be92348400799617e7b9f9e121d610b4020
Urichcool <104870932+Urichcool@users.noreply.github.com>
2023-05-13 00:39:00 +0200

    added fetch function

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { Product } from './components/product';
-import { products } from './data/products';
-
+import React, { useEffect, useState } from "react";
+import { Product } from "./components/product";
+import { IProduct } from "./models";
+import axios, { AxiosError } from "axios";
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState("");
+
+  const fetchProducts = async () => {
+    try {
+      setIsError("");
+      setIsLoading(true);
+      const response = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products?limit=5"
+      );
+      setProducts(response.data);
+      setIsLoading(false);
+    } catch (e: unknown) {
+      setIsLoading(false);
+      const error = e as AxiosError;
+      setIsError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+      {isLoading && <p className="text-center">Loading...</p>}
+      {isError && <p className="text-center text-red-600">{isError}</p>}
+      {products.map((product) => (
+        <Product product={product} key={product.id} />
+      ))}
+      {/* <Product product={products[0]} />
+      <Product product={products[1]} /> */}
     </div>
   );
 }
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,16 +1,38 @@
 import { IProduct } from "../models";
+import { useState } from "react";
 
 interface ProductProps {
-    product: IProduct
+  product: IProduct;
 }
 
+export const Product = ({ product }: ProductProps) => {
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
 
-export const Product = ({product}: ProductProps) => {
-    return (
-      <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
-        <img src={product.image} className="w-1/6" alt={product.title}></img>
-            <p>{product.title}</p>
-            <span className="font-bold">{product.price} $</span>
+  const btnBgClassName = isButtonClicked ? 'bg-yellow-400' : 'bg-blue-400'
+  const btnClasses = ['py-2 px-4 border', btnBgClassName]
+
+const handleButtonToggle = () => {
+  setIsButtonClicked(!isButtonClicked)
+}
+
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      <img src={product.image} className="w-1/6" alt={product.title}></img>
+      <p>{product.title}</p>
+      <p className="font-bold">{product.price} $</p>
+      <button className={btnClasses.join(" ")} onClick={handleButtonToggle}>
+        {!isButtonClicked ? "Show details" : "Hide details"}
+      </button>
+      <div>
+        {isButtonClicked && (
+          <>
+            <p>{product.description}</p>
+            <p>
+              Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span>
+            </p>
+          </>
+        )}
       </div>
-    );
-}
+    </div>
+  );
+};
